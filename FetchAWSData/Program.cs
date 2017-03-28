0a780872c53c5c2b6c41080f7776bd64fdee9c30
Abhishek <abhishekds1992@gmail.com>
2017-03-27 22:03:17 -0400

    Start Phase with file reader

---
+++ FetchAWSData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FetchAWSData
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> awsUrl = new List<string>();
+
+            using (var reader = new StreamReader(@"C:\Users\Abhishek\Desktop\test.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    awsUrl.Add(line);
+                }
+            }
+            string[] urlArray = awsUrl.ToArray();
+ 
+            Console.WriteLine(urlArray[2]);
+            Console.ReadKey();
+        }
+    }
+}
